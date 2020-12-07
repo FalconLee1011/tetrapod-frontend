e193cf9ca9b712d3a9469fe94ae0a78ef867bc99
falcon 草 <falcon.lee1011@gmail.com>
2020-12-07 15:49:51 +0800

    fix: counter stepper adjusment

--- src/components/misc/countstepper.vue
+++ src/components/misc/countstepper.vue
@@ -1,20 +1,44 @@
 <template>
-<div style="border: 1px solid gray;width:100px;border-radius: 5px;">
-    <button style="width:40px;font-size:large;" :disabled="isButtonDisabled" @click="n -= 1">-</button>{{n}}<button style="width:40px;font-size:large;" @click="n += 1">+</button>
-</div>
+  <v-text-field
+    v-model="n"
+    prepend-inner-icon="mdi-minus"
+    append-icon="mdi-plus"
+    outlined 
+    dense
+    class="stepper"
+    @click:prepend-inner="min"
+    @click:append="add"
+  ></v-text-field>
 </template>
 
-<script>
-  export default {
-      name: 'countstepper',
-      props:{howmany:Number,},
-      data: () => ({
-      n:1,
-      }),
-      computed:{
-        isButtonDisabled() {
-          return this.n==1;
-          }
-      },
+<style lang="css">
+  .stepper{
+    width: 7.5rem !important; 
   }
+  .stepper input{
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+  }
+</style>
+
+<script>
+export default {
+  name: "countstepper",
+  props: { howmany: Number },
+  data: () => ({
+    n: 1,
+  }),
+  methods: {
+    add(){
+      this.n += 1;
+      this.$emit("add")
+    },
+    min(){
+      if(this.n > 1) this.n -= 1;
+      this.$emit("min")
+    },
+  },
+  
+};
 </script>
